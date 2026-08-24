bd08e0a8d92c51a222ec5d00d707f345b56365f2
peuhkk <ph6353752@gmail.com>
2026-08-24 17:30:55 -0300

    v0.1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║     SISTEMA DE ESTOQUE       ║");
+        System.out.println("╠══════════════════════════════╣");
+
+
+    }
+}
