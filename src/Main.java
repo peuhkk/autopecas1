2744281eb62a48004e4a3915bdbfad1f175423ac
peuhkk <ph6353752@gmail.com>
2026-08-24 18:13:49 -0300

    v0.1 - Primeira Versão! Parte de cadastro de peça, utilizando: Entrada e saída de dados e variáveis.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[]args){
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         System.out.println("╔══════════════════════════════╗");
-        System.out.println("║     SISTEMA DE ESTOQUE       ║");
+        System.out.println("║      SISTEMA DE ESTOQUE      ║");
         System.out.println("╠══════════════════════════════╣");
+        System.out.println("║       CADASTRE A PEÇA        ║");
+        System.out.println("╚══════════════════════════════╝");
+
+        System.out.print("Nome da peça: ");
+        String nomePeca = sc.nextLine();
+        System.out.print("Quantidade à adicionar ao estoque: ");
+        int quantidadeAdd = sc.nextInt();
+        System.out.print("Valor da peça: ");
+        double valorPeca = sc.nextDouble();
+        System.out.print("Identificação: ");
+        int idPeca = sc.nextInt();
+
+        System.out.println("╔═══════════════════════════════╗");
+        System.out.println("║  Peça cadastrada com Sucesso! ║");
+        System.out.println("╚═══════════════════════════════╝");
+        System.out.println("====== Informações da peça ======");
+        System.out.println("Nome: " + nomePeca);
+        System.out.println("Valor da peca: "+ valorPeca);
+        System.out.println("ID da Peça: " + idPeca);
+        System.out.println("Estoque atualizado: "+ quantidadeAdd);
+
 
     }
 }
