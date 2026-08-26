c795c7e634e41726d5e7da959a7fc9107b96bcf3
peuhkk <ph6353752@gmail.com>
2026-08-25 23:06:33 -0300

    v0.2 - Versão com o fluxo funcionando perfeitamente!

--- src/Menu.java
+++ src/Menu.java
@@ -7,28 +7,34 @@ public class Menu {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        byte escolha = 0;
 
-        if (escolha != 2) {
+        String[] nomePeca = new String[100];
+        int[] quantidadeAdd = new int[100];
+        double[] valorPeca = new double[100];
+        int[] idPeca = new int[100];
+
+        String escolha = "1";
+
+       while(escolha.equals("1")) {
             System.out.println("╔══════════════════════════════╗");
             System.out.println("║      SISTEMA DE ESTOQUE      ║");
             System.out.println("╠══════════════════════════════╣");
             System.out.println("║                              ║");
-            System.out.println("║  1 -      Cadastro           ║");
-            System.out.println("║  2 -        SAIR             ║");
+            System.out.println("║  1 -      Menu               ║");
+            System.out.println("║  2 -      Cadastro           ║");
+            System.out.println("║  3 -      Sair               ║");
             System.out.println("║                              ║");
             System.out.println("╚══════════════════════════════╝");
 
-            escolha = sc.nextByte();
+            escolha = sc.next();
+            if (escolha.equals("3")){
+                break;
+            }
             sc.nextLine();
 
-            String[] nomePeca = new String[100];
-            int[] quantidadeAdd = new int[100];
-            double[] valorPeca = new double[100];
-            int[] idPeca = new int[100];
 
-            if (escolha == 1) {
-                for (int i = 0; i < 1; i++) {
+            if (escolha.equals("2")) {
+                for (int i = 0; escolha.equals("2"); i++) {
                     System.out.println("╔══════════════════════════════╗");
                     System.out.println("║       CADASTRE A PEÇA        ║");
                     System.out.println("╚══════════════════════════════╝");
@@ -52,10 +58,11 @@ public class Menu {
                     System.out.println("ID da Peça: " + idPeca[i]);
                     System.out.println("Estoque atualizado: " + quantidadeAdd[i]);
 
-                    System.out.println("Você deseja voltar? digite 2");
-                    escolha = sc.nextByte();
+                    System.out.println("");
+                    System.out.println("Para voltar digite 1!");
+                    System.out.println("Para cadastrar outro, digite 2");
+                    escolha = sc.nextLine();
                 }
-
             }
 
         }
