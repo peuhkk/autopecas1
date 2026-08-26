38457e867431f18c0e607765cfc111e038398645
peuhkk <ph6353752@gmail.com>
2026-08-25 22:10:26 -0300

    v0.1 - Primeira Versão! Teste v2

--- src/Menu.java
+++ src/Menu.java
@@ -8,56 +8,56 @@ public class Menu {
         Scanner sc = new Scanner(System.in);
 
         byte escolha = 0;
-        String escolhaSair = " ";
 
-        while (escolhaSair != "sim") {
-        System.out.println("╔══════════════════════════════╗");
-        System.out.println("║      SISTEMA DE ESTOQUE      ║");
-        System.out.println("╠══════════════════════════════╣");
-        System.out.println("║                              ║");
-        System.out.println("║  1 - BUSCA                   ║");
-        System.out.println("║  2 - CADASTRO                ║");
-        System.out.println("║  3 - SAIR                    ║");
-        System.out.println("║                              ║");
-        System.out.println("╚══════════════════════════════╝");
+        if (escolha != 2) {
+            System.out.println("╔══════════════════════════════╗");
+            System.out.println("║      SISTEMA DE ESTOQUE      ║");
+            System.out.println("╠══════════════════════════════╣");
+            System.out.println("║                              ║");
+            System.out.println("║  1 -      Cadastro           ║");
+            System.out.println("║  2 -        SAIR             ║");
+            System.out.println("║                              ║");
+            System.out.println("╚══════════════════════════════╝");
 
-        escolha = sc.nextByte();
-        sc.nextLine();
-// FASE DE TESTE
+            escolha = sc.nextByte();
+            sc.nextLine();
+
+            String[] nomePeca = new String[100];
+            int[] quantidadeAdd = new int[100];
+            double[] valorPeca = new double[100];
+            int[] idPeca = new int[100];
+
             if (escolha == 1) {
-                while (escolhaSair != "sim") {
-                    System.out.println("Digite o ID para buscar a peça");
-                    byte busca = sc.nextByte();
+                for (int i = 0; i < 1; i++) {
+                    System.out.println("╔══════════════════════════════╗");
+                    System.out.println("║       CADASTRE A PEÇA        ║");
+                    System.out.println("╚══════════════════════════════╝");
+
+                    System.out.print("Nome da peça: ");
+                    nomePeca[i] = sc.nextLine();
+                    System.out.print("Quantidade à adicionar ao estoque: ");
+                    quantidadeAdd[i] = sc.nextInt();
+                    System.out.print("Valor da peça: ");
+                    valorPeca[i] = sc.nextDouble();
+                    System.out.print("Identificação: ");
+                    idPeca[i] = sc.nextInt();
                     sc.nextLine();
-                    System.out.println("Deseja sair?");
-                    escolhaSair = sc.nextLine();
+
+                    System.out.println("╔═══════════════════════════════╗");
+                    System.out.println("║  Peça cadastrada com Sucesso! ║");
+                    System.out.println("╚═══════════════════════════════╝");
+                    System.out.println("====== Informações da peça ======");
+                    System.out.println("Nome: " + nomePeca[i]);
+                    System.out.printf("Valor da peca: R$ %.2f%n", valorPeca[i]);
+                    System.out.println("ID da Peça: " + idPeca[i]);
+                    System.out.println("Estoque atualizado: " + quantidadeAdd[i]);
+
+                    System.out.println("Você deseja voltar? digite 2");
+                    escolha = sc.nextByte();
                 }
+
             }
 
-
-            if (escolha == 2) {
-                System.out.println("╔══════════════════════════════╗");
-                System.out.println("║       CADASTRE A PEÇA        ║");
-                System.out.println("╚══════════════════════════════╝");
-
-                System.out.print("Nome da peça: ");
-                String nomePeca = sc.nextLine();
-                System.out.print("Quantidade à adicionar ao estoque: ");
-                int quantidadeAdd = sc.nextInt();
-                System.out.print("Valor da peça: ");
-                double valorPeca = sc.nextDouble();
-                System.out.print("Identificação: ");
-                int idPeca = sc.nextInt();
-
-                System.out.println("╔═══════════════════════════════╗");
-                System.out.println("║  Peça cadastrada com Sucesso! ║");
-                System.out.println("╚═══════════════════════════════╝");
-                System.out.println("====== Informações da peça ======");
-                System.out.println("Nome: " + nomePeca);
-                System.out.printf("Valor da peca: R$ %.2f%n", valorPeca);
-                System.out.println("ID da Peça: " + idPeca);
-                System.out.println("Estoque atualizado: " + quantidadeAdd);
-            }
         }
     }
 }
